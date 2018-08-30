ef4a69b98cdd6eb51e5ef32a8cb3e5d5a1081e7c
Amberight <41237397+Amberight@users.noreply.github.com>
2018-08-30 21:58:47 +0300

    Added multi-room functionality

--- ChatServer/Server.cpp
+++ ChatServer/Server.cpp
@@ -3,9 +3,21 @@
 #include "time.h"
 #include <thread>
 #include <fstream>
+#include <vector>
 
 static char client1[20]; //Globals
 static char client2[20];
+std::vector<SOCKET> Clients;
+std::vector<std::string> clientNames;
+SOCKADDR_IN ADDRESS;
+int(AddressSize) = sizeof(ADDRESS);
+int unassignedCleints = 0;
+int clientCounter = 0;
+std::vector<std::thread> threads;
+
+long SUCCESSFUL; //Declaring essentials
+WSAData WinSockData;
+WORD DLLVERSION;
 
 Server::Server()
 {
@@ -13,67 +25,65 @@ Server::Server()
 
 void Server::setUp()
 {
-	long SUCCESSFUL; //Declaring essentials
-	WSAData WinSockData;
-	WORD DLLVERSION;
-
 	DLLVERSION = MAKEWORD(2, 1);
 
 	SUCCESSFUL = WSAStartup(DLLVERSION, &WinSockData);
-
-	SOCKADDR_IN ADDRESS;
-	int(AddressSize) = sizeof(ADDRESS);
 
 	ADDRESS.sin_addr.s_addr = inet_addr("127.0.0.1");
 	ADDRESS.sin_family = AF_INET;
 	ADDRESS.sin_port = htons(444);
 
 	SOCKET listenSocket; //Listeining socket and client connection sockets
-	SOCKET connection1;
-	SOCKET connection2;
 
 	listenSocket = socket(AF_INET, SOCK_STREAM, NULL);
 	bind(listenSocket, (SOCKADDR*)&ADDRESS, sizeof(ADDRESS));
-	listen(listenSocket, 2); //Listeing to 2 connections
+	listen(listenSocket, SOMAXCONN); //Listeing socket
 
-
-
-	for (;;)
-	{
-		printf("\n\tWaiting for both clients to connect.. ");
-		if (connection1 = accept(listenSocket, (SOCKADDR*)&ADDRESS, &AddressSize)); //Waits for first client to connect and recieves
-		{
-			SUCCESSFUL = recv(connection1, client1, sizeof(client1), NULL);
-			std::string tempClient = client1;
-
-			printf("\nFirst client named %s %s", client1, "is present, waiting for client two, to connect...");
-
-			for (;;)
-			{
-				if (connection2 = accept(listenSocket, (SOCKADDR*)&ADDRESS, &AddressSize)); //Waits for second client
-				{
-					SUCCESSFUL = recv(connection2, client2, sizeof(client2), NULL); //Recieves and saves name of the second client
-					std::string tempClient = client2;
-
-					printf("\nSecond client named %s %s", client2, "is present, continuing... ");
-
-					SUCCESSFUL = send(connection1, client2, 20, NULL); //Sends names to clients
-					SUCCESSFUL = send(connection2, client1, 20, NULL);
-
-					
-					std::thread transfer1(&Server::msgTransfer, connection1, connection2, client1, client2); //Starts main message exchange function
-					std::thread transfer2(&Server::msgTransfer, connection2, connection1, client2, client1);
-					
-					transfer1.join();
-					transfer2.join();
-				}
-				break;
-			}
-			break;
-		}
-	}
+	std::thread clientCollector(&Server::clientGet, listenSocket);
 
 	
+	Server::chatRoom();	
+}
+
+void Server::chatRoom()
+{
+	while (true)
+	{
+		if (unassignedCleints == 2)
+		{
+			int First = clientCounter - 2;
+			int Second = clientCounter - 1;
+			char firstName[20];
+			char secondName[20];
+			strcpy_s(firstName, sizeof(firstName), clientNames[First].c_str());
+			strcpy_s(secondName, sizeof(secondName), clientNames[Second].c_str());
+			SUCCESSFUL = send(Clients[First], secondName, 20, NULL);
+			SUCCESSFUL = send(Clients[Second], firstName, 20, NULL);
+
+			threads.push_back(std::thread(&Server::msgTransfer, Clients[First], Clients[Second], firstName, secondName));
+			threads.push_back(std::thread(&Server::msgTransfer, Clients[Second], Clients[First], secondName, firstName));
+			unassignedCleints = 0;
+		}
+	}
+}
+
+void Server::clientGet(SOCKET &Lis)
+{
+	while (true)
+	{
+		SOCKET TempClient;
+		char TempName[20];
+		if (TempClient = accept(Lis, (SOCKADDR*)&ADDRESS, &AddressSize))
+		{
+			Clients.push_back(TempClient);
+			SUCCESSFUL = recv(Clients[clientCounter], TempName, sizeof(client1), NULL);
+			clientNames.push_back(TempName);
+
+			unassignedCleints++;
+			clientCounter++;
+			Sleep(500);
+		}
+	}
 }
 
 void Server::msgTransfer(SOCKET &Sender, SOCKET &Reciever, char cliName1[20], char cliName2[20])
